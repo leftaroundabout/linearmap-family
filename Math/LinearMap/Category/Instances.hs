989b1f041fe280a25f6d7dafd4c21e57d166a858
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-01-20 16:08:00 +0100

    `TensorSpace` instance for finite-supported sequences.

--- Math/LinearMap/Category/Instances.hs
+++ Math/LinearMap/Category/Instances.hs
@@ -40,12 +40,15 @@ import Data.Tagged
 import Data.Foldable (foldl')
 
 import Data.VectorSpace.Free
+import Data.VectorSpace.Free.FiniteSupportedSequence
 import qualified Linear.Matrix as Mat
 import qualified Linear.Vector as Mat
 import qualified Linear.Metric as Mat
 import Linear ( V0(V0), V1(V1), V2(V2), V3(V3), V4(V4)
               , _x, _y, _z, _w )
 import Control.Lens ((^.))
+
+import qualified Data.Vector.Unboxed as UArr
 
 import Math.LinearMap.Asserted
 import Math.VectorSpace.ZeroDimensional
@@ -249,5 +252,37 @@ instance (Fractional' n, TensorProduct (DualVector n) n ~ n)
 
 
 
+instance (Num' n, UArr.Unbox n) => Semimanifold (FinSuppSeq n) where
+  type Needle (FinSuppSeq n) = FinSuppSeq n
+  (.+~^) = (.+^); translateP = Tagged (.+^)
+  toInterior = pure; fromInterior = id
 
+instance (Num' n, UArr.Unbox n) => PseudoAffine (FinSuppSeq n) where
+  v.-~.w = Just $ v.-.w; (.-~!) = (.-.)
 
+instance (Num' n, UArr.Unbox n) => TensorSpace (FinSuppSeq n) where
+  type TensorProduct (FinSuppSeq n) v = [v]
+  wellDefinedVector (FinSuppSeq v) = FinSuppSeq <$> UArr.mapM wellDefinedVector v
+  scalarSpaceWitness = case closedScalarWitness :: ClosedScalarWitness n of
+        ClosedScalarWitness -> ScalarSpaceWitness
+  linearManifoldWitness = LinearManifoldWitness BoundarylessWitness
+  zeroTensor = Tensor []
+  toFlatTensor = LinearFunction $ Tensor . UArr.toList . getFiniteSeq
+  fromFlatTensor = LinearFunction $ FinSuppSeq . UArr.fromList . getTensorProduct
+  addTensors (Tensor s) (Tensor t) = Tensor $ Mat.liftU2 (^+^) s t
+  scaleTensor = bilinearFunction $ \μ (Tensor t) -> Tensor $ (μ*^)<$>t
+  negateTensor = LinearFunction $ \(Tensor t) -> Tensor $ negateV<$>t
+  tensorProduct = bilinearFunction
+                    $ \(FinSuppSeq v) w -> Tensor $ (*^w)<$>UArr.toList v
+  transposeTensor = LinearFunction $ \(Tensor a)
+    -> let n = length a
+       in foldl' (^+^) zeroV
+        $ zipWith ( \i w -> (getLinearFunction tensorProduct w) $ FinSuppSeq
+                              $ UArr.generate n (\j -> if i==j then 1 else 0) )
+             [0..] a
+  fmapTensor = bilinearFunction $ \f (Tensor a) -> Tensor $ map (f$) a
+  fzipTensorWith = bilinearFunction $ \f (Tensor a, Tensor b)
+                     -> Tensor $ zipWith (curry $ arr f) a b
+  coerceFmapTensorProduct _ Coercion = Coercion
+  wellDefinedTensor (Tensor a) = Tensor <$> Hask.traverse wellDefinedVector a
+  
