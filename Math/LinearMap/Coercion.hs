de9639e3fa1716a5750508079e85631e06529a1d
Justus Sagemüller <jsag@hvl.no>
2022-09-09 16:03:08 +0200

    Module for the new coercion type.

---
+++ Math/LinearMap/Coercion.hs
@@ -0,0 +1,15 @@
+-- |
+-- Module      : Math.LinearMap.Coercion
+-- Copyright   : (c) Justus Sagemüller 2022
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) jsag $ hvl.no
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+module Math.LinearMap.Coercion
+   ( VSCCoercion(..)
+   ) where
+
+import Math.LinearMap.Category.Class
