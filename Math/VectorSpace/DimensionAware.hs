a4e94e69828ba2cf963e2a367c63ad56d67252e4
Justus Sagemüller <jsag@hvl.no>
2022-10-23 20:15:05 +0200

    One more type class.
    For spaces that actually have a fixed/finite/static dimension.

--- Math/VectorSpace/DimensionAware.hs
+++ Math/VectorSpace/DimensionAware.hs
@@ -9,16 +9,21 @@
 -- 
 
 
-{-# LANGUAGE TypeFamilies         #-}
-{-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE DefaultSignatures    #-}
-{-# LANGUAGE RankNTypes           #-}
-{-# LANGUAGE UnicodeSyntax        #-}
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE AllowAmbiguousTypes  #-}
-{-# LANGUAGE TypeApplications     #-}
-{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE GADTs                  #-}
+{-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE PolyKinds              #-}
+{-# LANGUAGE DefaultSignatures      #-}
+{-# LANGUAGE RankNTypes             #-}
+{-# LANGUAGE UnicodeSyntax          #-}
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE UndecidableInstances   #-}
+{-# LANGUAGE AllowAmbiguousTypes    #-}
+{-# LANGUAGE TypeApplications       #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+{-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE FunctionalDependencies #-}
 
 module Math.VectorSpace.DimensionAware where
 
@@ -31,6 +36,12 @@ import GHC.TypeLits
 import Data.Ratio
 
 import qualified Math.VectorSpace.DimensionAware.Theorems.MaybeNat as Maybe
+
+
+data DimensionalityWitness v where
+  IsStaticDimensional :: n`Dimensional`v => DimensionalityWitness v
+  IsFlexibleDimensional :: StaticDimension v ~ 'Nothing => DimensionalityWitness v
+
 
 -- | This class does not really pose any restrictions on a vector space type, but
 --   allows it to express its dimension.
@@ -48,17 +59,54 @@ class VectorSpace v => DimensionAware v where
                => Sing (StaticDimension v)
   staticDimensionSing = sing
 
+  dimensionalityWitness :: DimensionalityWitness v
 
-instance DimensionAware Float   where type StaticDimension Float   = 'Just 1
-instance DimensionAware Double  where type StaticDimension Double  = 'Just 1
-instance DimensionAware Int     where type StaticDimension Int     = 'Just 1
-instance DimensionAware Integer where type StaticDimension Integer = 'Just 1
+
+instance DimensionAware Float   where
+  type StaticDimension Float   = 'Just 1
+  dimensionalityWitness = IsStaticDimensional
+instance DimensionAware Double  where
+  type StaticDimension Double  = 'Just 1
+  dimensionalityWitness = IsStaticDimensional
+instance DimensionAware Int     where
+  type StaticDimension Int     = 'Just 1
+  dimensionalityWitness = IsStaticDimensional
+instance DimensionAware Integer where
+  type StaticDimension Integer = 'Just 1
+  dimensionalityWitness = IsStaticDimensional
 instance Integral n => DimensionAware (Ratio n) where
   type StaticDimension (Ratio n) = 'Just 1
+  dimensionalityWitness = IsStaticDimensional
 
 instance ∀ u v . (DimensionAware u, DimensionAware v, Scalar u ~ Scalar v)
                    => DimensionAware (u,v) where
   type StaticDimension (u,v) = Maybe.ZipWithPlus (StaticDimension u) (StaticDimension v)
   staticDimensionSing = Maybe.zipWithPlusSing (staticDimensionSing @u)
                                               (staticDimensionSing @v)
+  dimensionalityWitness = case (dimensionalityWitness @u, dimensionalityWitness @v) of
+    (IsStaticDimensional, IsStaticDimensional) -> IsStaticDimensional
+    (IsFlexibleDimensional, _) -> IsFlexibleDimensional
+    (_, IsFlexibleDimensional) -> IsFlexibleDimensional
+
+
+class (DimensionAware v, StaticDimension v ~ 'Just n)
+           => n`Dimensional`v | v -> n
+
+  
+instance 1`Dimensional`Float   where
+instance 1`Dimensional`Double  where
+instance 1`Dimensional`Int     where
+instance 1`Dimensional`Integer where
+instance Integral n => 1`Dimensional`Ratio n where
+
+  
+instance ∀ n u m v nm . ( n`Dimensional`u, m`Dimensional`v
+                        , Scalar u ~ Scalar v
+                        , nm ~ (n+m) )
+                   => nm`Dimensional`(u,v) where
+
+type family FromJust (a :: Maybe k) :: k where
+  FromJust ('Just v) = v
+
+type StaticallyKnownDimension v = FromJust (StaticDimension v)
 
