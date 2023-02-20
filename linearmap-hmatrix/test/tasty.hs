a05b6c32c3a2e72585f4008a0acc09e91521b882
Justus Sagemüller <jsag@hvl.no>
2023-02-20 19:49:15 +0100

    Some very basic tests.

--- linearmap-hmatrix/test/tasty.hs
+++ linearmap-hmatrix/test/tasty.hs
@@ -39,6 +39,10 @@ import Math.VectorSpace.DimensionAware (toArray, fromArray, unsafeFromArray)
 import Math.Manifold.Core.Types
 import Math.Manifold.Core.PseudoAffine
 
+import GHC.TypeLits (KnownNat, natVal)
+import Data.Proxy (Proxy(..))
+import Data.Function (on)
+
 import Test.Tasty
 import Test.Tasty.QuickCheck
 import qualified Test.QuickCheck as QC
@@ -50,10 +54,24 @@ import qualified Data.Vector.Unboxed as UArr
 main :: IO ()
 main = do
   defaultMain $ testGroup "Tests"
-   [
+   [ testGroup "Vector-space basics"
+    [ testProperty "Addition associativity"
+       $ \v w x -> (v^+^w)^+^x ≈≈≈ v^+^(w^+^x :: R 27)
+    , testProperty "Addition commutativity"
+       $ \v w -> v^+^w === (w^+^v :: R 39)
+    , testProperty "Scalar distributivity"
+       $ \μ v w -> μ*^(v^+^w) ≈≈≈ μ*^v ^+^ μ*^(w :: R 92)
+    ]
    ]
 
 
+instance ∀ n . KnownNat n => Arbitrary (R n) where
+  arbitrary = HMatS.fromList <$> vectorOf n arbitrary
+   where n = fromIntegral $ natVal (Proxy @n)
+instance ∀ n . KnownNat n => Eq (R n) where
+  (==) = (==)`on`HMatS.extract
+
+infix 4 ≈≈≈
 (≈≈≈) :: (InnerSpace v, Show v, Eq v, RealFrac (Scalar v))
             => v -> v -> QC.Property
 v≈≈≈w
