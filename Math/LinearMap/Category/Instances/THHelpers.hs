ffae84fffd523fdf709a34a29909211adb876e0b
Justus Sagemüller <justussa@kth.se>
2025-05-26 00:31:24 +0200

    Port the `FreeLinearSpace` CPP macro to Template Haskell.
    This avoids much of the boilerplate for each of the V0 ... V4 types, as well as
    the problem that CPP does not have consistent semantics on different platforms.

--- Math/LinearMap/Category/Instances/THHelpers.hs
+++ Math/LinearMap/Category/Instances/THHelpers.hs
@@ -28,6 +28,7 @@
 
 module Math.LinearMap.Category.Instances.THHelpers
         ( mkLinearScalarSpace
+        , mkFreeLinearSpace
         , autoLinearManifoldWitness
         ) where
 
@@ -79,6 +80,7 @@ import qualified GHC.Exts as GHC
 import qualified GHC.Generics as GHC
 
 import Language.Haskell.TH
+import THLego.Helpers (multiAppE)
 
 import Data.Singletons (SingI, sing, Sing)
 #if MIN_VERSION_singletons(3,0,0)
@@ -150,3 +152,197 @@ mkLinearScalarSpace sName = do
     |]
 
 
+{-# INLINE tensorUnsafeFromArrayWithOffsetViaList #-}
+tensorUnsafeFromArrayWithOffsetViaList
+          :: ∀ v w n m α . ( n`Dimensional`v
+                           , m`Dimensional`w
+                           , Scalar v ~ Scalar w
+                           , GArr.Vector α (Scalar v) )
+   => ([w] -> TensorProduct v w) -> Int -> α (Scalar v) -> (v⊗w)
+tensorUnsafeFromArrayWithOffsetViaList l2v i ar
+   = Tensor $ l2v [ unsafeFromArrayWithOffset
+                      (i + j * dimension @w) ar
+                  | j <- [0 .. dimension @v - 1] ]
+
+{-# INLINE tensorUnsafeWriteArrayWithOffsetViaList #-}
+tensorUnsafeWriteArrayWithOffsetViaList
+        :: ∀ v w n m α σ . ( n`Dimensional`v
+                           , m`Dimensional`w
+                           , Scalar v ~ Scalar w
+                           , GArr.Vector α (Scalar v) )
+   => (TensorProduct v w -> [w]) -> GArr.Mutable α σ (Scalar v)
+          -> Int -> (v⊗w) -> ST σ ()
+tensorUnsafeWriteArrayWithOffsetViaList v2l ar i (Tensor t)
+   = forM_ (zip [0..] $ v2l t) $ \(j, v)
+       -> unsafeWriteArrayWithOffset ar
+                      (i + j * dimension @w) v
+
+mkFreeLinearSpace :: Name -> Name -> Int -> Q [Dec]
+mkFreeLinearSpace vTCstrName vECstrName dimens = do
+  let 
+      vTCstr = pure . ConT $ vTCstrName
+      -- vECstr = pure . ConE $ vECstrName
+      vExprn xVars = multiAppE (ConE vECstrName) $ VarE<$>xVars
+      vPatt xVars = ConP vECstrName [] $ VarP<$>xVars
+
+      dim = pure . LitT . NumTyLit $ fromIntegral dimens
+      newCVars sym = forM [0 .. dimens-1] $ newName . (sym:) . show
+      
+      vFromList, vToList, tensProd, tensId
+        , contraction, contraaction :: Q Exp
+
+      -- fromList [x₀, x₁, ...] = Vₙ x₀ x₁ ...
+      vFromList = do
+         xVars <- newCVars 'x'
+         pure . LamE [ListP (VarP<$>xVars)] $ vExprn xVars
+
+      -- toList (Vₙ x₀ x₁ ...) = [x₀, x₁, ...]
+      vToList = do
+         xVars <- newCVars 'x'
+         return . LamE [vPatt xVars]
+              $ ListE (VarE<$>xVars)
+
+      -- tensProd (Tensor (Vₙ x₀ x₁ ...)) = x₀⊗Vₙ 1 0 ...
+      --                                ^+^ x₁⊗Vₙ 0 1 ...
+      --                                ^+^ ...
+      tensProd = do
+         xVars <- newCVars 'x'
+         return . LamE [ConP 'Tensor [] [vPatt xVars]]
+              $ foldr (AppE . AppE (VarE '(^+^)))
+                      (VarE 'zeroV)
+                      [ multiAppE (VarE '(⊗))
+                         [ VarE xVar
+                         , multiAppE (ConE vECstrName)
+                                      [LitE . IntegerL
+                                         $ if i==j then 1 else 0
+                                      | j <- [0 .. dimens-1]] ]
+                      | (i,xVar) <- zip [0..] xVars ]
+       
+      -- tensId = Vₙ (\w -> Vₙ w     zeroV zeroV ...)
+      --             (\w -> Vₙ zeroV w     zeroV ...)
+      --             (\w -> Vₙ zeroV zeroV w     ...)
+      --             ...
+      tensId = do
+         multiAppE (ConE vECstrName) <$> forM [0 .. dimens-1] `id` \i -> do
+            wVar <- newName "w"
+            return . LamE [VarP wVar]
+              $ multiAppE (ConE vECstrName)
+                   [ VarE $ if i==j
+                      then wVar
+                      else 'zeroV
+                   | j <- [0 .. dimens-1] ]
+      
+      -- contraction ( Vₙ (Vₙ w₀ _ _ ...)
+      --                  (Vₙ _ w₁ _ ...)
+      --                  (Vₙ _ _ w₂ ...)
+      --                  ...             ) = w₀ ^+^ w₁ ^+^ w₂ ^+^ ...
+      contraction = do
+         wVars <- newCVars 'w'
+         return . LamE [ConP vECstrName []
+                         [ ConP vECstrName []
+                             [ if i==j
+                                then VarP wVar
+                                else WildP
+                             | j <- [0 .. dimens-1] ]
+                         | (i,wVar) <- zip [0..] wVars ]]
+           $ foldr (AppE . AppE (VarE '(^+^)))
+                   (VarE 'zeroV)
+                   [ VarE wVar
+                   | wVar <- wVars ]
+
+      -- contraaction (Vₙ x₀ x₁ ...) f
+      --          = (f$x₀)^._x + (f$x₁)^._y + ...
+      contraaction = do
+         xVars <- newCVars 'x'
+         fVar <- newName "f"
+         LamE [vPatt xVars, VarP fVar]
+             . foldr (AppE . AppE (VarE '(+))) (LitE $ IntegerL 0)
+             <$> forM (zip [0..] xVars) `id` \(i,xVar) -> do
+            rVar <- newName "r"
+            return
+             $ AppE (LamE [ConP vECstrName []
+                            [ if i==j
+                               then VarP rVar
+                               else WildP
+                            | j <- [0 .. dimens-1] ]]
+                          $ VarE rVar)
+                    (AppE (VarE '($)) (VarE fVar) `AppE` VarE xVar)
+
+
+  [d| instance Num s => Semimanifold ($vTCstr s) where
+        type Needle ($vTCstr s) = $vTCstr s
+        (.+~^) = (^+^)
+#if !MIN_VERSION_manifolds_core(0,6,0)
+        toInterior = pure
+        fromInterior = id
+        translateP = Tagged (^+^)
+#endif
+      instance Num s => PseudoAffine ($vTCstr s) where
+        v.-~.w = pure (v^-^w); (.-~!) = (^-^)
+      instance ∀ s . (Num' s, Eq s) => DimensionAware ($vTCstr s) where
+        type StaticDimension ($vTCstr s) = 'Just $dim
+        dimensionalityWitness = IsStaticDimensional
+      instance ∀ s . (Num' s, Eq s) => $dim `Dimensional` $vTCstr s where
+        unsafeFromArrayWithOffset
+           = unsafeFromArrayWithOffsetViaList $vFromList
+        unsafeWriteArrayWithOffset
+           = unsafeWriteArrayWithOffsetViaList $vToList
+      instance ∀ s . (Num' s, Eq s) => TensorSpace ($vTCstr s) where
+        type TensorProduct ($vTCstr s) w = $vTCstr w
+        scalarSpaceWitness = case closedScalarWitness :: ClosedScalarWitness s of
+                               ClosedScalarWitness -> ScalarSpaceWitness
+        linearManifoldWitness = autoLinearManifoldWitness
+        zeroTensor = Tensor $ pure zeroV
+        addTensors (Tensor m) (Tensor n) = Tensor $ liftA2 (^+^) m n
+        subtractTensors (Tensor m) (Tensor n) = Tensor $ liftA2 (^-^) m n
+        negateTensor = LinearFunction $ Tensor . fmap negateV . getTensorProduct
+        scaleTensor = bilinearFunction
+                $ \μ -> Tensor . fmap (μ*^) . getTensorProduct
+        toFlatTensor = case closedScalarWitness :: ClosedScalarWitness s of
+                               ClosedScalarWitness -> LinearFunction $ follow Tensor
+        fromFlatTensor = case closedScalarWitness :: ClosedScalarWitness s of
+                               ClosedScalarWitness -> LinearFunction $ flout Tensor
+        tensorProduct = bilinearFunction $ \w v -> Tensor $ fmap (*^v) w
+        transposeTensor = LinearFunction $tensProd
+        fmapTensor = bilinearFunction $
+                \(LinearFunction f) -> pretendLike Tensor $ fmap f
+        fzipTensorWith = bilinearFunction $
+                \(LinearFunction f) (Tensor vw, Tensor vx)
+                        -> Tensor $ liftA2 (curry f) vw vx
+        tensorUnsafeFromArrayWithOffset
+           = tensorUnsafeFromArrayWithOffsetViaList $vFromList
+        tensorUnsafeWriteArrayWithOffset
+           = tensorUnsafeWriteArrayWithOffsetViaList $vToList
+        coerceFmapTensorProduct _ VSCCoercion = Coercion
+        wellDefinedTensor = getTensorProduct >>> Hask.traverse wellDefinedVector
+                             >>> fmap Tensor
+      instance ∀ s . (Num' s, Eq s) => LinearSpace ($vTCstr s) where
+        type DualVector ($vTCstr s) = $vTCstr s
+        dualSpaceWitness = case closedScalarWitness :: ClosedScalarWitness s of
+               ClosedScalarWitness -> DualSpaceWitness
+        linearId = LinearMap Mat.identity
+        idTensor = Tensor Mat.identity
+        tensorId = ti dualSpaceWitness where
+           ti :: ∀ w . (LinearSpace w, Scalar w ~ s) => DualSpaceWitness w
+                      -> ($vTCstr s⊗w)+>($vTCstr s⊗w)
+           ti DualSpaceWitness = LinearMap $
+                fmap (\f -> fmap (LinearFunction $ Tensor . f)-+$>asTensor $ id)
+                     ($tensId :: $vTCstr (w -> $vTCstr w))
+        coerceDoubleDual = VSCCoercion
+        fromLinearForm = case closedScalarWitness :: ClosedScalarWitness s of
+              ClosedScalarWitness -> LinearFunction $ flout LinearMap
+        contractTensorMap = LinearFunction $ $contraction . coerce . getLinearMap
+        contractMapTensor = LinearFunction $ $contraction . coerce . getTensorProduct
+        contractLinearMapAgainst = bilinearFunction $ getLinearMap >>> $contraaction
+        applyDualVector = bilinearFunction Mat.dot
+        applyLinear = bilinearFunction $ \(LinearMap m)
+                        -> foldl' (^+^) zeroV . liftA2 (^*) m
+        applyTensorFunctional = bilinearFunction $ \(LinearMap f) (Tensor t)
+                   -> sum $ liftA2 (<.>^) f t
+        applyTensorLinMap = bilinearFunction $ \(LinearMap f) (Tensor t)
+                   -> foldl' (^+^) zeroV $ liftA2 ((fromTensor-+$=>) >>>
+                               getLinearFunction . getLinearFunction applyLinear) f t
+        composeLinear = bilinearFunction $
+               \f (LinearMap g) -> LinearMap $ fmap ((applyLinear-+$>f)-+$>) g
+        useTupleLinearSpaceComponents _ = usingNonTupleTypeAsTupleError
+   |]
